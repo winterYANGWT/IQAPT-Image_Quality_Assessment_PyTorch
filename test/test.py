ee1e31dbb3ad1076a7f00df296df83ccd341b30c
yangwentao <Winteryan1998@gmail.com>
2022-03-26 05:47:11 +0800

    add gpu test

--- test/test.py
+++ test/test.py
@@ -1,12 +1,7 @@
-import sys
 import os
 import os.path as path
 from prettytable import PrettyTable
 import functools
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../src')
-
-import torch
 import torchvision as tv
 import iqapt
 from PIL import Image
@@ -15,6 +10,8 @@ import numpy as np
 import piq
 from file_dict import *
 import lpips
+
+CUDA=True
 
 os.chdir(path.dirname(path.abspath(__file__)))
 open_images_from_dir = lambda dir: [
@@ -28,9 +25,10 @@ open_images = lambda d: {
 }
 
 dict_map = lambda d, func: {name: func(value) for (name, value) in d.items()}
+list_map=lambda l,func:[func(value) for value in l]
 
 # transfer func
-t_image2tensor = tv.transforms.ToTensor()
+t_image2tensor = tv.transforms.Compose([tv.transforms.ToTensor(),tv.transforms.Lambda(lambda img:img.cuda())]) if CUDA==True else tv.transforms.ToTensor()
 image2tensor = lambda image: t_image2tensor(image).unsqueeze(0)
 image2np = lambda image: np.array(image) / 255.0
 
@@ -39,6 +37,9 @@ def test_MSE():
     print('MSE: ')
     iqapt_mse = iqapt.MSE()
     table = PrettyTable(['ref_image', 'test_image', 'iqapt', 'scikit'])
+
+    if CUDA==True:
+        iqapt_mse=iqapt_mse.cuda()
 
     for p_images in MSE_IMAGES:
         images = open_images(p_images)
@@ -66,6 +67,10 @@ def test_PSNR():
         'ref_image', 'test_image', 'iqapt_gray', 'piq_gray', 'iqapt', 'piq',
         'scikit'
     ])
+
+    if CUDA==True:
+        iqapt_psnr=iqapt_psnr.cuda()
+        iqapt_psnr_g=iqapt_psnr_g.cuda()
 
     for p_images in PSNR_IMAGES:
         images = open_images(p_images)
@@ -96,6 +101,9 @@ def test_SSIM():
     piq_ssim = piq.ssim
     table = PrettyTable(['ref_image', 'test_image', 'iqapt', 'piq', 'scikit'])
 
+    if CUDA==True:
+        iqapt_ssim=iqapt_ssim.cuda()
+
     for p_images in SSIM_IMAGES:
         images = open_images(p_images)
         tensor_images = dict_map(images, image2tensor)
@@ -106,7 +114,7 @@ def test_SSIM():
                               tensor_images['test']).item()
         scikit_result = metrics.structural_similarity(np_images['ref'],
                                                       np_images['test'],
-                                                      multichannel=True,
+                                                      channel_axis=2,
                                                       win_size=11,
                                                       data_range=1,
                                                       gaussian_weights=True)
@@ -127,6 +135,10 @@ def test_LPIPS():
     lpips_lpips = lpips.LPIPS(net='vgg')
     table = PrettyTable(
         ['ref_image', 'test_image', 'iqapt', 'piq', 'lpips(official code)'])
+
+    if CUDA==True:
+        iqapt_lpips=iqapt_lpips.cuda()
+        lpips_lpips=lpips_lpips.cuda()
 
     for p_images in LPIPS_IMAGES:
         images = open_images(p_images)
@@ -154,11 +166,14 @@ def test_FID():
     piq_fid = piq.FID()
     table = PrettyTable(['ref_dir', 'test_dir', 'iqapt', 'piq'])
 
+    if CUDA==True:
+        iqapt_fid=iqapt_fid.cuda()
+
     src_images = open_images_from_dir('./Image/FID/source_images')
     trgt_images = open_images_from_dir('./Image/FID/generated_images')
 
-    src_images = list(map(t_image2tensor, src_images))
-    trgt_images = list(map(t_image2tensor, trgt_images))
+    src_images = list_map(src_images,t_image2tensor)
+    trgt_images = list_map(trgt_images,t_image2tensor)
 
     iqapt_result, features_a, features_b = iqapt_fid(src_images, trgt_images)
     iqapt_result = iqapt_result.item()
